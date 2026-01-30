6ce3b68eb175d65f8342634f02ccf4513d5873de
Dera3294 <dera.rajaonarimanana@gmail.com>
2026-01-30 15:18:57 +0300

    first commit

--- FrontOffice/src/main/java/frontOffice/frontend/FrontendApplication.java
+++ FrontOffice/src/main/java/frontOffice/frontend/FrontendApplication.java
@@ -2,12 +2,19 @@ package frontOffice.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class FrontendApplication {
+public class FrontendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FrontendApplication.class);
 	}
 
 }
